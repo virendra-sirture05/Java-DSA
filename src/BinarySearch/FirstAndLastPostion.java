334b2e85ca5e0ca9194f6e6dd93592f9bb1d3997
Virendra Sirture <virendrasirture05@gmail.com>
2024-07-16 23:26:42 +0530

    newOne

---
+++ src/BinarySearch/FirstAndLastPostion.java
@@ -0,0 +1,132 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class FirstAndLastPostion {
+
+    // method : 1
+    static int[] serachRange(int[] arr, int target){
+
+        // check if ans exist or not
+        int start = 0;
+        int end = arr.length - 1;
+        int[] ans = {-1,-1};
+        boolean flag = false;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                flag = true;
+                break;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        if(flag == false){
+            return ans;
+        }
+
+
+        // for lowerbound, first postion
+        start = 0;
+        end = arr.length - 1;
+        int lb = arr.length;
+
+        while (start <= end) {
+            int mid = start+(end-start)/2;
+            if(arr[mid] >= target){
+                lb = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        ans[0] = lb;
+
+
+        // for upper bound, last position
+        start = 0;
+        end = arr.length - 1;
+        int ub = arr.length;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] > target){
+                ub = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        ans[1] = ub - 1;
+
+        return ans;
+    }
+
+
+    
+
+    // method : 2
+    static int[] serachRange1(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        int[] ans = new int[2];
+        int fp = -1;
+
+        while (start <= end) {
+            int mid = start+(end-start)/2;
+            if(arr[mid] == target){
+                if(arr[mid] == arr[mid-1]){
+                    end = mid - 1;
+                }
+                else{
+                    fp = mid;
+                    break;
+                }
+            }
+            else if(arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        ans[0] = fp;
+
+        start = 0;
+        end = arr.length - 1;
+        int lp = -1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+
+            if(arr[mid]==target){
+                if(arr[mid] == arr[mid+1]){
+                    start = mid + 1;
+                }
+                else{
+                    lp = mid;
+                    break;
+                }
+            }
+            else if(arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        ans[1] = lp;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,20,20,20,20,20,40,50,60};
+        //    1, 4
+
+        int target = 20;
+
+        int[] ans = serachRange(arr, target);
+        System.out.println(Arrays.toString(ans));
+        int[] ans1 = serachRange(arr, target);
+        System.out.println(Arrays.toString(ans1));
+
+    }
+}
---
+++ src/BinarySearch/LowerBound.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70};
+        int x = 35;
+        int lb = arr.length;
+
+        int start = 0;
+        int end = arr.length -1;
+
+        while (start <= end) {
+            int mid = start + (end -start)/2;
+
+            if(arr[mid] >= x){
+                lb = Math.min(mid, lb);
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(lb);
+    }
+}
