64438c986714e6a41ff3a300339dea7a4b056473
Virendra Sirture <virendrasirture05@gmail.com>
2024-07-16 23:36:37 +0530

    new

--- src/BinarySearch/FirstAndLastPostion.java
+++ src/BinarySearch/FirstAndLastPostion.java
@@ -117,6 +117,41 @@ public class FirstAndLastPostion {
         ans[1] = lp;
         return ans;
     }
+
+    // method: 3
+    static int[] serachRange2(int[] arr, int target){
+        int ans[] = {-1,-1};
+        int firstOcc = search(arr, target, true);
+        int lastOcc = search(arr, target, false);
+
+        ans[0] = firstOcc;
+        ans[1] = lastOcc;
+        return ans;
+    }
+    static int search(int[] arr, int target, boolean isFirst){
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = arr.length;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] == target){
+                ans = mid;
+                if(isFirst){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else {
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr = {10,20,20,20,20,20,40,50,60};
         //    1, 4
@@ -127,6 +162,8 @@ public class FirstAndLastPostion {
         System.out.println(Arrays.toString(ans));
         int[] ans1 = serachRange(arr, target);
         System.out.println(Arrays.toString(ans1));
+        int[] ans2 = serachRange(arr, target);
+        System.out.println(Arrays.toString(ans2));
 
     }
 }
